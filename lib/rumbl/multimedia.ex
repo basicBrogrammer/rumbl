d5ef26cad42866f26ddc502078533b2d12011e6f
Jeremy Ward <jeremy.ward@mckesson.com>
2019-12-01 18:28:06 -0500

    [7] Ecto Queries and Constraints

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -7,6 +7,7 @@ defmodule Rumbl.Multimedia do
   alias Rumbl.Repo
 
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Multimedia.Category
   alias Rumbl.Accounts
 
   @doc """
@@ -71,6 +72,16 @@ defmodule Rumbl.Multimedia do
     |> Video.changeset(attrs)
     |> Ecto.Changeset.put_assoc(:user, user)
     |> Repo.insert()
+  end
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
+  end
+
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
   end
 
   @doc """
